a4d77fea984fcc6819883a291998c818080aeb8d
Puneet <puneet19338@gmail.com>
2024-04-09 14:25:20 +0530

    new

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,27 @@
-// import {initServer} from "./app";
+import {initServer} from "./app";
 
 
 
 
-// async function init(){
-//     const app = await initServer();
-//     app.listen(8000,()=>{
-//         console.log("start");
-//     })
-// }
-
-// init();
-import { initServer } from "./app";
-
-async function init() {
+async function init(){
     const app = await initServer();
-    const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 10000; // Use port 10000 by default if environment variable PORT is not set
-
-    app.listen(PORT, '0.0.0.0', () => {
-        console.log(`Server running at http://0.0.0.0:${PORT}`);
-    });
+    const PORT: number = process.env.PORT ? parseInt(process.env.PORT) :8000
+    app.listen(PORT,()=>{
+        console.log("start");
+    })
 }
 
 init();
+// import { initServer } from "./app";
+
+// async function init() {
+//     const app = await initServer();
+//     const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 10000; // Use port 10000 by default if environment variable PORT is not set
+
+//     app.listen(PORT, '0.0.0.0', () => {
+//         console.log(`Server running at http://0.0.0.0:${PORT}`);
+//     });
+// }
+
+// init();
 
