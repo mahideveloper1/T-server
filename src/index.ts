7e97ef192b8d16bf25e29191b986e4673eeabfbd
Puneet <puneet19338@gmail.com>
2024-04-11 10:25:51 +0530

    only prisma

--- src/index.ts
+++ src/index.ts
@@ -12,16 +12,5 @@ async function init(){
 }
 
 init();
-// import { initServer } from "./app";
 
-// async function init() {
-//     const app = await initServer();
-//     const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 10000; // Use port 10000 by default if environment variable PORT is not set
-
-//     app.listen(PORT, '0.0.0.0', () => {
-//         console.log(`Server running at http://0.0.0.0:${PORT}`);
-//     });
-// }
-
-// init();
-
+// "dev": "npx prisma migrate deploy && tsc-watch --onSuccess \"npm start\""
